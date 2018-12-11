52719fca11d02fa1ce8c57652b02a01e9415949f
Marek Wojciechowski <mrkwjc@gmail.com>
2018-12-11 06:47:01 +0100

    Update version to 4.0.7-1

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ else:
 
 if __name__ == "__main__":
     server = 'http://gmsh.info/bin'
-    version = '4.0.6'
-    iversion = version+'-2'  # installer number
+    version = '4.0.7'
+    iversion = version+'-1'  # installer number
 
     system = platform.system().lower()
     machine = platform.machine().lower()
