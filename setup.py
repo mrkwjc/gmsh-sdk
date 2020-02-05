44b3c9b430ef1f14e3c4fda75216d8663b3e918d
Marek Wojciechowski <mrkwjc@users.noreply.github.com>
2020-02-05 10:00:53 +0100

    Update to version 4.5.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 if __name__ == "__main__":
     server = 'http://gmsh.info/bin'
-    version = '4.5.1'
+    version = '4.5.2'
     iversion = version+'-1'  # installer number
 
     system = platform.system().lower()
