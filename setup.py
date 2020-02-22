f819600823d62a3abce142744ea398ebec993a31
Marek Wojciechowski <mrkwjc@gmail.com>
2020-02-22 21:28:02 +0100

    Back to distutils

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import platform
 import tarfile
 import zipfile
 from distutils import sysconfig
-from setuptools import setup
+from distutils.core import setup
 if sys.version_info.major == 2:
     from urllib2 import urlopen
 else:
